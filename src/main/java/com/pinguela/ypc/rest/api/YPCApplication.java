93a6420636212b0b6cef79e86eb32fd23ea2d3eb
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-07 15:30:31 +0100

    Add schema converter for AttributeDTO

--- src/main/java/com/pinguela/ypc/rest/api/YPCApplication.java
+++ src/main/java/com/pinguela/ypc/rest/api/YPCApplication.java
@@ -2,6 +2,10 @@ package com.pinguela.ypc.rest.api;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.pinguela.ypc.rest.api.json.AttributeMapperContextResolver;
+import com.pinguela.ypc.rest.api.schema.AttributeModelConverter;
+
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
@@ -16,7 +20,9 @@ public class YPCApplication extends ResourceConfig {
 
 	public YPCApplication() {
 		packages(YPCApplication.class.getPackage().getName());
+		
 		register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
+		ModelConverters.getInstance().addConverter(new AttributeModelConverter());
 		
 		register(AttributeMapperContextResolver.class);
 	}
