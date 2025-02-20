597cb4c258ad730711d85620a4b42e6bd540cc97
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-21 00:02:05 +0100

    Add mappings for value handling modes

--- src/main/java/com/pinguela/ypc/rest/api/constants/AttributeJsonMappings.java
+++ src/main/java/com/pinguela/ypc/rest/api/constants/AttributeJsonMappings.java
@@ -1,14 +1,18 @@
 package com.pinguela.ypc.rest.api.constants;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.pinguela.yourpc.model.constants.AttributeDataTypes;
+import com.pinguela.yourpc.model.constants.AttributeValueHandlingModes;
 
-public class AttributeJsonTypes {
+public class AttributeJsonMappings {
 	
 	private static final BiMap<String, String> TYPE_MAPPINGS;
+	private static final Map<Integer, String> HANDLING_MODE_MAPPINGS;
 	
 	static {
 		TYPE_MAPPINGS = HashBiMap.create();
@@ -16,6 +20,10 @@ public class AttributeJsonTypes {
 		TYPE_MAPPINGS.put(AttributeDataTypes.VARCHAR, "string");
 		TYPE_MAPPINGS.put(AttributeDataTypes.DECIMAL, "double");
 		TYPE_MAPPINGS.put(AttributeDataTypes.BOOLEAN, "boolean");
+		
+		HANDLING_MODE_MAPPINGS = new HashMap<>();
+		HANDLING_MODE_MAPPINGS.put(AttributeValueHandlingModes.RANGE, "range");
+		HANDLING_MODE_MAPPINGS.put(AttributeValueHandlingModes.SET, "set");
 	}
 	
 	public static String getJsonType(String dataType) {
@@ -24,6 +32,10 @@ public class AttributeJsonTypes {
 	
 	public static String getDataType(String jsonType) {
 		return TYPE_MAPPINGS.inverse().get(jsonType);
+	}
+	
+	public static String getHandlingMode(Integer handlingMode) {
+		return HANDLING_MODE_MAPPINGS.get(handlingMode);
 	}
 	
 	public static Set<String> getAllJsonTypes() {
