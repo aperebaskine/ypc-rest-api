666384dae3110c43d89ef9224399faf4067603ca
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-20 15:31:49 +0100

    Handle short representation of attribute

--- src/main/java/com/pinguela/ypc/rest/api/json/serialize/AttributeDeserializer.java
+++ src/main/java/com/pinguela/ypc/rest/api/json/serialize/AttributeDeserializer.java
@@ -2,20 +2,35 @@ package com.pinguela.ypc.rest.api.json.serialize;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Locale;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.pinguela.DataException;
+import com.pinguela.ServiceException;
 import com.pinguela.yourpc.model.dto.AttributeDTO;
+import com.pinguela.yourpc.service.AttributeService;
+import com.pinguela.yourpc.service.impl.AttributeServiceImpl;
 import com.pinguela.ypc.rest.api.constants.AttributeJsonTypes;
 
 @SuppressWarnings("serial")
 public class AttributeDeserializer extends StdDeserializer<AttributeDTO<?>> {
+	
+	private static Logger logger = LogManager.getLogger(AttributeDeserializer.class);
+	
+	// TODO: Cache values
+	private AttributeService attributeService;
 
 	public AttributeDeserializer() {
 		super(AttributeDTO.class);
+		this.attributeService = new AttributeServiceImpl();
 	}
 
 	@Override
@@ -23,8 +38,20 @@ public class AttributeDeserializer extends StdDeserializer<AttributeDTO<?>> {
 			throws IOException, JacksonException {
 
 		JsonNode root = (JsonNode) p.readValueAsTree();
-		String jsonType = root.get("dataType").asText();
-		String dataType = AttributeJsonTypes.getDataType(jsonType);
+		
+		Integer id = root.get("id").asInt();
+		
+		JsonNode dataTypeNode = root.get("dataType");
+		String dataType;
+		try {
+			dataType = dataTypeNode == null ? 
+					attributeService.findById(id, Locale.forLanguageTag("en-GB"), false, null).getDataTypeIdentifier() :
+					AttributeJsonTypes.getDataType(dataTypeNode.asText());
+		} catch (ServiceException | DataException e) {
+			String errorMsg = String.format("Exception thrown while fetching attribute data: %s", e.getMessage());
+			logger.error(errorMsg, e);
+			throw new JsonMappingException(p, errorMsg);
+		}
 
 		AttributeDTO<?> dto = AttributeDTO.getInstance(dataType);
 		dto.setId(root.get("id").asInt());
