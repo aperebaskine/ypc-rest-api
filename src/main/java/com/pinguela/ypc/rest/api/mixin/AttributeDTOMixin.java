efffad4294559e91198666f17a8da1a5f4b276cc
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-21 13:53:41 +0100

    Update schema and JSON annotations

--- src/main/java/com/pinguela/ypc/rest/api/mixin/AttributeDTOMixin.java
+++ src/main/java/com/pinguela/ypc/rest/api/mixin/AttributeDTOMixin.java
@@ -2,6 +2,7 @@ package com.pinguela.ypc.rest.api.mixin;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -9,6 +10,7 @@ import com.pinguela.yourpc.model.dto.AttributeValueDTO;
 import com.pinguela.ypc.rest.api.json.serialize.AttributeDataTypeSerializer;
 import com.pinguela.ypc.rest.api.json.serialize.AttributeHandlingModeSerializer;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "Attribute")
@@ -19,21 +21,22 @@ public abstract class AttributeDTOMixin {
 
 	@JsonProperty
 	private String name;
-
-	@JsonProperty("dataType")
+	
+	@JsonProperty
+	@ArraySchema(schema = @Schema(implementation = AttributeValueDTO.class))
+	private List<AttributeValueDTO<?>> values;
+	
+	@JsonGetter("dataType")
 	@JsonSerialize(using = AttributeDataTypeSerializer.class)
 	abstract String getDataTypeIdentifier();
-
-	@JsonProperty
-	private List<AttributeValueDTO<?>> values; 
+	
+	@Schema(type = "string")
+	@JsonGetter("handlingMode")
+	@JsonSerialize(using = AttributeHandlingModeSerializer.class)
+	abstract Integer getValueHandlingMode();
 
 	@JsonIgnore
 	abstract List<AttributeValueDTO<?>> getValuesByHandlingMode();
-
-	@Schema(type = "string")
-	@JsonProperty("handlingMode")
-	@JsonSerialize(using = AttributeHandlingModeSerializer.class)
-	abstract Integer getValueHandlingMode();
 
 	@JsonIgnore
 	abstract Class<?> getTypeParameterClass();
