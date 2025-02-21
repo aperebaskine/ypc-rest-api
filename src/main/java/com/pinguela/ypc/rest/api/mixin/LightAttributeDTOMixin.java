edf1d3f597d07f69b443a265a20de4784fc96c81
Alexandre Perebaskine <pereb27@gmail.com>
2025-02-21 13:53:15 +0100

    Update schema annotations

--- src/main/java/com/pinguela/ypc/rest/api/mixin/LightAttributeDTOMixin.java
+++ src/main/java/com/pinguela/ypc/rest/api/mixin/LightAttributeDTOMixin.java
@@ -5,14 +5,16 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pinguela.yourpc.model.dto.AttributeValueDTO;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(name = "Attribute (Light)")
+@Schema(name = "LightAttribute")
 public class LightAttributeDTOMixin {
 	
 	@JsonProperty
 	private Integer id;
 	
 	@JsonProperty
+	@ArraySchema(schema = @Schema(implementation = AttributeValueDTO.class))
 	private List<AttributeValueDTO<?>> values;
 }
