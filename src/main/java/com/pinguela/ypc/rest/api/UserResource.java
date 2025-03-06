e641b113b41e30f437db4547b8422a41c716e6c4
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-06 11:11:03 +0100

    WIP UserResource

---
+++ src/main/java/com/pinguela/ypc/rest/api/UserResource.java
@@ -0,0 +1,102 @@
+package com.pinguela.ypc.rest.api;
+
+import com.pinguela.InvalidLoginCredentialsException;
+import com.pinguela.YPCException;
+import com.pinguela.yourpc.model.Customer;
+import com.pinguela.yourpc.service.CustomerService;
+import com.pinguela.yourpc.service.impl.CustomerServiceImpl;
+import com.pinguela.ypc.rest.api.util.ResponseWrapper;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("/user")
+public class UserResource {
+
+	private CustomerService customerService;
+
+	public UserResource() {
+		this.customerService = new CustomerServiceImpl();
+	}
+
+	@POST
+	@Path("/login")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(
+			method = "POST",
+			operationId = "loginCustomer",
+			description = "Authenticates the customer, returning their info.", 
+			responses = {
+					@ApiResponse(
+							responseCode = "200", 
+							description = "Successfully logged in",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Customer.class)
+									)
+							), 
+					@ApiResponse(
+							responseCode = "404",
+							description = "Customer not found"
+							),
+					@ApiResponse(
+							responseCode = "400",
+							description = "Error in request"
+							)
+			})
+	public Response login(
+			@FormParam("email") @Email @NotNull String email,
+			@FormParam("password") @NotNull String password
+			) {
+
+		Customer c;
+
+		try {
+			c = customerService.login(email, password);
+
+		} catch (InvalidLoginCredentialsException e) {
+			return Response.status(Status.NOT_FOUND).build();
+		} catch (YPCException e) {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+
+		return ResponseWrapper.wrap(() -> c);
+	}
+
+	@POST
+	@Path("/register")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(
+			method = "POST",
+			operationId = "registerCustomer",
+			description = "Create a customer account", 
+			responses = {
+					@ApiResponse(
+							responseCode = "200", 
+							description = "Successfully registered.",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Customer.class)
+									)
+							),
+					@ApiResponse(
+							responseCode = "400",
+							description = "Error in request"
+							)
+			})
+	public Response register() {
+
+	}
+
+}
