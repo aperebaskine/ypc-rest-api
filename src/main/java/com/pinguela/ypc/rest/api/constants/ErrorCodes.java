9e0c19392974a78d42ee71bed2e7ad47e6a70607
Alexandre Perebaskine <pereb27@gmail.com>
2025-03-06 11:11:28 +0100

    Add error codes

---
+++ src/main/java/com/pinguela/ypc/rest/api/constants/ErrorCodes.java
@@ -0,0 +1,26 @@
+package com.pinguela.ypc.rest.api.constants;
+
+public final class ErrorCodes {
+
+	private ErrorCodes() {
+	}
+	
+	public static final String UNKNOWN_ERROR = "UNKNOWN_ERROR";
+	
+	public static final String INVALID_LOGIN = "INVALID_LOGIN";
+	
+	public static final String INVALID_LENGTH = "INVALID_LENGTH";
+	public static final String INVALID_FORMAT = "INVALID_FORMAT";
+	
+	public static final String PASSWORDS_DO_NOT_MATCH = "PASSWORDS_DO_NOT_MATCH";
+	
+	public static final String NOT_A_NUMBER = "NOT_A_NUMBER";
+	public static final String NOT_A_DATE = "NOT_A_DATE";
+	
+	public static final String NON_EXISTENT_CATEGORY = "NON_EXISTENT_CATEGORY";
+	public static final String MISSING_PARAMETER_VALUE = "MISSING_PARAMETER_VALUE";
+	
+	public static final String EMAIL_IN_USE = "EMAIL_IN_USE";
+	public static final String PHONE_NUMBER_IN_USE = "PHONE_NUMBER_IN_USE";
+	
+}
