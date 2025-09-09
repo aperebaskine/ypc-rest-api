decbfe71c2af51960c0ec7fa11ee3c77ba9ddff9
Alexandre Perebaskine <pereb27@gmail.com>
2025-09-09 15:26:41 +0200

    Add constant for application/jwt media type

---
+++ src/main/java/com/pinguela/ypc/rest/api/constants/MediaType.java
@@ -0,0 +1,10 @@
+package com.pinguela.ypc.rest.api.constants;
+
+public final class MediaType {
+
+	private MediaType() {
+	}
+	
+	public static final String APPLICATION_JWT = "application/jwt";
+
+}
