813aa571b275259e7f74ecbfaa482fb0cce55997
Alexandre Perebaskine <pereb27@gmail.com>
2025-09-09 16:29:13 +0200

    Fix: Remove unused import

--- src/main/java/com/pinguela/ypc/rest/api/CustomerResource.java
+++ src/main/java/com/pinguela/ypc/rest/api/CustomerResource.java
@@ -22,7 +22,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
